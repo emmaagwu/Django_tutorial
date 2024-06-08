3517ed0770353d1ef38b30b45ab8f6e3c738d9a6
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-08 16:19:15 +0100

    Add initial Django project files and create polls app

---
+++ mysite/polls/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+
+from .models import Question
+
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = { "latest_question_list": latest_question_list}
+  return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  context = { "question": question }
+  return render(request, "polls/detail.html", context)
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return render(response % question_id)
+
+def vote(request, question_id):
+  return render("You're voting on question %s." % question_id)
